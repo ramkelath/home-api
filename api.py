3c79716fe460594eea83778648a1165a6467b77c
Everyone <everyone@Everyones-iMac.local>
2020-10-18 19:13:42 -0400

    Version of code ready to be submitted.

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
 import flask
+from flask import request, jsonify
+import requests
+import json
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
 
 @app.route('/', methods=['GET'])
 def home():
-    return "{septic:true}"
+    return 'An api that tells you whether or not a home has a septic tank'
 
-app.run()
+@app.route('/api', methods=['GET'])
+def api():
+    endpoint = "http://127.0.0.1:5001/canaryapi"
+    response = requests.get(endpoint, params = {'address': request.args.get('address'), 'zipcode':request.args.get('zipcode')})
+    homedata = json.loads(response.text)
+    if (homedata["sewer"] == 'septic'):
+      return jsonify({"septic":"true"})
+    else:
+      return jsonify({"septic":"false"})
+
+app.run(host='127.0.0.1',port=5000)
