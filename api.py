30a177673b8faf098d74c88b5a7bdcde525e5e40
Everyone <everyone@Everyones-iMac.local>
2020-10-20 12:27:13 -0400

    Added authentication to API request.

--- api.py
+++ api.py
@@ -5,18 +5,21 @@ import json
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return 'An api that tells you whether or not a home has a septic tank'
 
-@app.route('/api', methods=['GET'])
+@app.route('/api', methods=['GET','POST'])
 def api():
     endpoint = "http://127.0.0.1:5001/canaryapi"
-    response = requests.get(endpoint, params = {'address': request.args.get('address'), 'zipcode':request.args.get('zipcode')})
-    homedata = json.loads(response.text)
-    if (homedata["sewer"] == 'septic'):
-      return jsonify({"septic":"true"})
+    response = requests.post(endpoint, auth = ('apikey', 'apisecret'), params = {'address': request.args.get('address'), 'zipcode':request.args.get('zipcode')})
+    if (response.status_code == 200):
+        homedata = json.loads(response.text)
+        if (homedata["sewer"] == 'septic'):
+          return jsonify({"septic":"true"})
+        else:
+          return jsonify({"septic":"false"})
     else:
-      return jsonify({"septic":"false"})
+        return "Invalid response from Canary API"
 
 app.run(host='127.0.0.1',port=5000)
--- mockcanary.py
+++ mockcanary.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, abort
 import requests
 
 app = flask.Flask(__name__)
@@ -9,12 +9,15 @@ app.config["DEBUG"] = True
 def home():
     return 'An api that mimics, very minimally, the canary home api'
 
-@app.route('/canaryapi', methods=['GET'])
+@app.route('/canaryapi', methods=['GET','POST'])
 def canary():
+    auth = request.authorization
+    if (auth.username != 'apikey' or auth.password != 'apisecret'):
+        abort(403)
     zip = int(request.args.get('zipcode'))
     if (zip % 6 == 0):
       return jsonify({"sewer":"septic"})
     else:
-      return jsonify({"sewer":"town"})
+      return jsonify({"sewer":"municipal"})
 
 app.run(host='127.0.0.1',port=5001)
